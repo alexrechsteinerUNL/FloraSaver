cb9904f684c136ad024400631d454e54f83c8481
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2022-10-31 22:31:13 -0500

    Added service for ANDROID ONLY. Windows iOS etc will fail. Service is still the default value from the stackoverflow page referenced.

--- FloraSaver/Views/MainPage.xaml.cs
+++ FloraSaver/Views/MainPage.xaml.cs
@@ -2,9 +2,23 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
+	IServiceNotification NotificationServices;
+	public MainPage(IServiceNotification _NotificationServices)
 	{
 		InitializeComponent();
-	}
+		NotificationServices = _NotificationServices;
+
+
+    }
+
+    private void OnServiceStartClicked(object sender, EventArgs e)
+    {
+        NotificationServices.Start();
+    }
+    //method to stop manually foreground service
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        NotificationServices.Stop();
+    }
 }
 
