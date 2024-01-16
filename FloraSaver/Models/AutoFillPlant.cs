f58be4e6c707d9d0f2eff6ad013103f5622fa3e5
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2024-01-16 15:01:51 -0600

    Trying to resolve notifications not working in release mode.
    I have also made a new object that will be the basis for the plant database of pre-found plants

---
+++ FloraSaver/Models/AutoFillPlant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraSaver.Models
+{
+    public class AutoFillPlant : Plant
+    {
+        string PlantSource { get; set; }
+
+        //maybe don't just do the base and instead use predefinied intervals to construct the plants when they are found from the database.
+        public AutoFillPlant(IPlant _Plant, string source = null) : base(_Plant)
+        {
+            if (!string.IsNullOrEmpty(source))
+            {
+                PlantSource = source;
+            }
+        }
+    }
+}
--- FloraSaver/Platforms/Android/MainApplication.cs
+++ FloraSaver/Platforms/Android/MainApplication.cs
@@ -4,6 +4,13 @@ using Android.Runtime;
 [assembly: UsesPermission(Android.Manifest.Permission.ReadMediaAudio)]
 [assembly: UsesPermission(Android.Manifest.Permission.ReadMediaImages)]
 [assembly: UsesPermission(Android.Manifest.Permission.ReadMediaVideo)]
+[assembly: UsesPermission(Android.Manifest.Permission.WakeLock)]
+
+//Required so that the plugin can reschedule notifications upon a reboot
+[assembly: UsesPermission(Android.Manifest.Permission.ReceiveBootCompleted)]
+[assembly: UsesPermission(Android.Manifest.Permission.Vibrate)]
+[assembly: UsesPermission("android.permission.SCHEDULE_EXACT_ALARM")]
+[assembly: UsesPermission("android.permission.POST_NOTIFICATIONS")]
 namespace FloraSaver;
 
 [Application]
