377684873fb7e236ceac52571f4d8d44f0d71db9
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2024-01-29 20:48:59 -0600

    Created a new SearchedPlant model that will be used to generate a score that will order a list of plants for when they are searched. #1 priority is search on plantname, #2 plant species, #3 Group, #4 Next Action,#5 birthday

---
+++ FloraSaver/Models/SearchedPlants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraSaver.Models
+{
+    class SearchedPlants : IPlant
+    {
+        SearchedPlants(IPlant _plant)
+        {
+            Id = _plant.Id;
+            PlantSpecies = _plant.PlantSpecies;
+            WaterInterval = _plant.WaterInterval;
+            MistInterval = _plant.MistInterval;
+            SunInterval = _plant.SunInterval;
+        }
+        SearchedPlants(IPlant _plant, string searchQuery)
+        {
+            Id = _plant.Id;
+            PlantSpecies = _plant.PlantSpecies;
+            WaterInterval = _plant.WaterInterval;
+            MistInterval = _plant.MistInterval;
+            SunInterval = _plant.SunInterval;
+        }
+
+        public int Id { get; set; }
+        public string PlantSpecies { get; set; }
+        public double? WaterInterval { get; set; }
+        public double? MistInterval { get; set; }
+        public double? SunInterval { get; set; }
+        public double SearchScore { get; set; } = 0.0;
+
+        public double GenerateSearchScore()
+        {
+            return (double)SearchScore;
+        }
+    }
+}
