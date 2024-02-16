e908a42f1aaee52af5cbac29bd2b1c6200752f1c
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2024-02-15 20:27:57 -0600

    The notification action can now be TAPPED, and it'll open up the table page. This is genuinely insane. Please ADD a query attribute to the table page that will scoll to the correct plant when tapped, and for the Multinotification have it open up the handing page.

--- FloraSaver/App.xaml.cs
+++ FloraSaver/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using FloraSaver.Services;
 using Microsoft.VisualStudio.Threading;
+using Plugin.LocalNotification;
+using Plugin.LocalNotification.EventArgs;
 
 namespace FloraSaver;
 
@@ -8,6 +10,28 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        LocalNotificationCenter.Current.NotificationActionTapped += OnNotificationActionTapped;
         MainPage = new AppShell();
+    }
+
+    private void OnNotificationActionTapped(NotificationActionEventArgs e)
+    {
+        if (e.IsDismissed)
+        {
+            // your code goes here
+            return;
+        }
+        if (e.IsTapped)
+        {
+            // your code goes here
+            Shell.Current.GoToAsync($"///{nameof(TablePage)}", true, new Dictionary<string, object>
+            {
+                {"ShouldGetNewData", true },
+                {"ShouldGetNewGroupData", true }
+            });
+            return;
+        }
+        // if Notification Action are setup
+        //switch (e.ActionId)
     }
 }
