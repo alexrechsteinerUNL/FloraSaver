92620e74f7563a3204130a31d7491eba0f7e3350
alexrechsteinerUNL <alex.rechsteiner@huskers.unl.edu>
2024-03-07 19:40:17 -0600

    Started building the PlantValiationArgs need to make sure each set has a message addition.

---
+++ FloraSaver/Models/PlantValidationArgs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloraSaver.Models
+{
+    public class PlantValidationArgs
+    {
+        public string Message { get; set; } = "";
+
+        public bool IsNameSectionInvalid { get; set; } = false;
+        public bool IsGroupSectionInvalid { get; set; } = false;
+        public bool IsSpeciesSectionInvalid { get; set; } = false;
+        public bool IsBirthdaySectionInvalid { get; set; } = false;
+        public bool IsDateTimeNextWateringSectionInvalid { get; set; } = false;
+
+        private bool _isDateTimeNextMistingSectionInvalid = false;
+        public bool IsDateTimeNextMistingSectionInvalid
+        {
+            get { return _isDateTimeNextMistingSectionInvalid; }
+            set
+            {
+                if (value)
+                {
+                    Message += "Next Misting time cannot be before Last Misting Time";
+                }
+                _isDateTimeNextMistingSectionInvalid = value;
+            }
+        }
+
+        private bool _isDateTimeNextMovingSectionInvalid = false;
+        public bool IsDateTimeNextMovingSectionInvalid { get { return _isDateTimeNextMovingSectionInvalid; }
+            set {
+            if (value)
+                {
+                    Message += "Next moving time cannot be before Last Moving Time";
+                }
+                _isDateTimeNextMovingSectionInvalid = value;
+            } 
+        }
+
+        public bool IsSuccessful {  get; set; } = false;
+
+        public PlantValidationArgs(Plant plant)
+        {
+            IsSpeciesSectionInvalid = string.IsNullOrEmpty(plant.PlantSpecies);
+            IsNameSectionInvalid = string.IsNullOrEmpty(plant.PlantGroupName);
+            IsGroupSectionInvalid = (string.IsNullOrEmpty(plant.PlantGroupName) || string.IsNullOrEmpty(plant.GroupColorHexString);
+            IsBirthdaySectionInvalid = CheckIfLastPlantDateIsFurtherThanPlantNextDate(plant.DateOfBirth, DateTime.Now);
+            IsDateTimeNextWateringSectionInvalid = CheckIfLastPlantDateIsFurtherThanPlantNextDate(plant.DateOfLastWatering, plant.DateOfNextWatering);
+            IsDateTimeNextMistingSectionInvalid = CheckIfLastPlantDateIsFurtherThanPlantNextDate(plant.DateOfLastMisting, plant.DateOfNextMisting);
+            IsDateTimeNextMovingSectionInvalid = CheckIfLastPlantDateIsFurtherThanPlantNextDate(plant.DateOfLastMove, plant.DateOfNextMove);
+
+        }
+
+        public bool CheckIfLastPlantDateIsFurtherThanPlantNextDate(DateTime lastDate, DateTime nextDate)
+        {
+            if (lastDate > nextDate) { return true; } else { return false; }
+        }
+
+
+    }
+}
